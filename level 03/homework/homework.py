3ca42c07eb94c808da5e72eec0cf746404b24bc1
aleksishirani <aleksi.shirani@eleve.efc.edu.ge>
2024-06-13 17:59:06 +0400

    commit

---
+++ level 03/homework/homework.py
@@ -0,0 +1,11 @@
+name = "aleksi"
+
+lastname = "shirani"
+
+hobby = "basketball"
+
+age = 13
+
+country = "georgia"
+
+print("My name is"+" "+name+" "+ "my lastname is"+" "+lastname+" "+"i am"+" "+str(age)+ " "+"my hobby is"+" "+hobby+" "+"i live in"+" "+country)
---
+++ level 03/homework/homework2.py
@@ -0,0 +1,9 @@
+print(10+2)
+
+print(10-2)
+
+print(10*2)
+
+print(10//2)
+
+print(10**2)
